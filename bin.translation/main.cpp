a498167f80000b676c24d241e78b38461cf1b2de
chirkovg <chirkovg@MacBook-Pro-Grigorij.local>
2015-04-10 10:51:52 +0300

    Initial Commit

---
+++ bin.translation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bin.translation
+//
+//  Created by Григорий Чирков on 10.04.15.
+//  Copyright (c) 2015 Григорий Чирков. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
