26aebf9ea3bc75b5ca3b883844c242fff3a63023
chirkovg <chirkovg@gmail.com>
2015-04-20 17:02:58 +0300

    work

--- bin.translation/header.cpp
+++ bin.translation/header.cpp
@@ -88,8 +88,8 @@ FILE* makeheader(const lexem* source)
     header -> cputype = CPU_TYPE_X86_64;
     header -> cpusubtype = CPU_SUBTYPE_X86_64_ALL;
     header -> filetype = MH_EXECUTE;
-    header -> ncmds = 1;
-    header -> sizeofcmds = 72;
+    header -> ncmds = 2;
+    header -> sizeofcmds = 224;
     header -> flags = MH_NOUNDEFS;
     header -> reserved = 0;
     fwrite(header, 1, sizeof(*header), res);
@@ -109,19 +109,41 @@ FILE* makeheader(const lexem* source)
     zeroseg -> flags = 0;
     fwrite(zeroseg, 1, sizeof(*zeroseg), res);
     
-    size_t textsegsize = textsize(source);
+    size_t textsectsize = textsize(source);
     struct segment_command_64* textseg = (struct segment_command_64*) calloc(1, sizeof(textseg));
     textseg -> cmd = LC_SEGMENT_64;
     textseg -> cmdsize = 152;
+    //textseg -> cmdsize = 72;
     memmove(textseg -> segname, SEG_TEXT, strlen(SEG_TEXT));
-    zeroseg -> vmaddr = 4096;
+    textseg -> vmaddr = 4096;
     uint64_t vmsize = 4096;
     
-    while (vmsize < textsegsize)
+    while (vmsize < textsectsize)
         vmsize *= 2;
-    zeroseg -> vmsize = vmsize;
-    zeroseg -> fileoff = 0;
-    zeroseg -> filesize = 
+    textseg -> vmsize = vmsize;
+    
+    textseg -> fileoff = 0;
+    textseg -> filesize = vmsize;
+    textseg -> maxprot = VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE;
+    textseg -> initprot = VM_PROT_READ | VM_PROT_EXECUTE;
+    textseg -> nsects = 1;
+    textseg -> flags = 0;
+    fwrite(textseg, 1, sizeof(*textseg), res);
+    
+    struct section_64* textsect = (struct section_64*) calloc(1, sizeof(textsect));
+    memmove(textsect -> sectname, SECT_TEXT, strlen(SECT_TEXT));
+    memmove(textsect -> segname, SEG_TEXT, strlen(SEG_TEXT));
+    textsect -> addr = ;
+    textsect -> size = textsectsize;
+    textsect -> offset = ;
+    textsect -> align = 0;
+    textsect -> reloff = 0;
+    textsect -> nreloc = 0;
+    textsect -> flags = S_REGULAR | S_ATTR_SOME_INSTRUCTIONS | S_ATTR_PURE_INSTRUCTIONS;
+    textsect -> reserved1 = 0;
+    textsect -> reserved2 = 0;
+    textsect -> reserved3 = 0;
+    
     
     
     return res;
